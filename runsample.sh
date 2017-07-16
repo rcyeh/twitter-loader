16350c195afb8015e6208577a513b17755648918
Richard C Yeh <richard.c.yeh+ec2@gmail.com>
2017-07-16 17:40:44 +0000

    added shutdown method

--- runsample.sh
+++ runsample.sh
@@ -9,9 +9,11 @@ if [ ! -d /mnt/NoBackup/twitter ]; then
 fi
 prefix=*tweets_
 s3bucket="s3://rcy-twitter/raw/aws/"
+notify="arn:aws:sns:us-east-1:780170686194:rcyeh-sms"
 
 i=0
 # look for existing files and set index to first unused
+echo "Looking for existing index high-water-mark ..."
 for f in ${outdir}/${prefix}*; do
   j=$(echo ${f/${outdir}\/${prefix}/} | sed -e "s/\..*$//")
   if (( ${i} < ${j} )); then
@@ -21,11 +23,17 @@ done
 
 cd ${outdir}
 while true; do
+  prev_i=${i}
   i=$(( ${i} + 1 ))
   echo "Writing to ${outdir}/${prefix}${i}.json.gz"
   python ${mydir}/sample_stock_tweets.py ${i}
-  for p in sample; do
-    aws s3 cp ${p}_tweets_${i}.json.gz ${s3bucket} --storage-class STANDARD_IA && rm -f ${p}_tweets_${i}.json.gz && touch uploaded_tweets_${i}.json.gz
-  done &
+  aws s3 cp sample_tweets_${i}.json.gz ${s3bucket} --storage-class STANDARD_IA && touch uploaded_tweets_${i}.json.gz && rm -f sample_tweets_${i}.json.gz uploaded_tweets_${prev_i}.json.gz &
+  if [ -f .shutdown ]; then
+    break
+  fi
   sleep 0.1
 done
+rm -f .shutdown
+message="twitter-loader on $(hostname) shut down at $(date)"
+aws sns publish --topic-arn "${notify}" --message "${message}" & 
+echo ${message}
