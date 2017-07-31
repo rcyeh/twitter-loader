2bac593a2f3965720c1a8d59eec793fe94d7329f
Richard C Yeh <Richard.C.Yeh@gmail.com>
2017-07-31 01:13:43 -0400

    corrected spot instance request, added setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+
+sudo yum update
+sudo yum install tmux
+
+aws configure
+
+aws s3 cp s3://rcy-twitter/root/,opt,anaconda.tgz .
+aws s3 cp s3://rcy-twitter/root/home.tgz .
+
+(cd /; sudo tar xvzf /home/ec2-user/,opt,anaconda.tgz )
+
+tar xvzf home.tgz
+
